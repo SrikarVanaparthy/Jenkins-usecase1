109533d01c474f4664964b6ca5966e89280dd497
SrikarVanaparthy <svanaparthy@anergroup.com>
2025-05-12 16:48:02 +0530

    Update users1.py

--- users1.py
+++ users1.py
@@ -1,4 +1,4 @@
-print("Exporting the users from github")
-print('@Navateja')
-print('@Srikar')
-print('@Chiranjeevi')
+print("Exporting the users from source system to end. ")
+# print('@Navateja')
+# print('@Srikar')
+# print('@Chiranjeevi')
